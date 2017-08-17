486ffc704acc66c1df97e1b8cab45d3d1d4384ee
toshiemon18 <st12318@gmail.com>
2017-08-18 02:18:15 +0900

    Implement ArXivAbstractTranslator module

--- toybox/arxiv_abst_translater.rb
+++ toybox/arxiv_abst_translater.rb
@@ -1,8 +1,12 @@
 # coding : utf-8
 
+$:.unshift(File.join(File.dirname(__FILE__), "lib"))
+
 require 'rubygems'
 require 'nokogiri'
-require "./lib/microsoft_translator"
+require "open-uri"
+require "net/https"
+require "microsoft_translator"
 
 # ToshiemonSlackBots
 #   This module is the namespace for toshiemon slack-bots definition
@@ -18,6 +22,23 @@ module ToshiemonSlackBots
       def initialize(url)
         @url = url
         @client = MicrosoftTranslator::Client.new
+      end
+
+      def run
+        abstract = fetch_abstract
+        @client.translate(abstract, to: "ja")
+      end
+
+      private
+      def fetch_html
+        html = Nokogiri::HTML.parse(open(@url))
+      end
+
+      def fetch_abstract
+        html = fetch_html
+        abstract = html.xpath("//*[@id=\"abs\"]/div[2]/blockquote/text()").text
+        # Eliminate new-line code and whitespace, hyphenations
+        abstract.gsub!("\n", " ").strip!.gsub!("- ", "")
       end
     end
   end
